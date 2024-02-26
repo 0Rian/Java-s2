563b45c200a50607ee835ff25b2440d1691ad88b
Rian <lianadrianrian@gmail.com>
2024-02-26 10:48:46 -0800

    Tugas

---
+++ src/main/java/Modul1/T1.java
@@ -0,0 +1,17 @@
+package org.Modul1;
+import java.util.Scanner;
+
+
+public class T1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("=====LONGI=====");
+        System.out.println("Daftar Pilihan :");
+        System.out.println("1. Sebagai Admin");
+        System.out.println("2. Sebagai Mahasiswa");
+        System.out.println("Masukan Pilihan Anda:");
+        String pilihan = scanner.nextLine();
+
+    }
+}
